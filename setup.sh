f4e11d7f42d1f8c188b831c8a0dcdc62998fc605
GetPsyched <priyanshu@getpsyched.dev>
2023-11-17 17:25:39 +0530

    refactor: store config in the right directory

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 echo "Setting up Google Drive Auth..."
 echo "Please check your browser"
-google-drive-ocamlfuse
+google-drive-ocamlfuse -xdgbd
 
 echo "Setting configuration options..."
-crudini --merge ~/.gdfuse/default/config < gdrive-ocamlfuse-config.ini
+crudini --merge ~/.config/gdfuse/default/config < gdrive-ocamlfuse-config.ini
 
 echo "Mouting the folder to harbour/..."
 google-drive-ocamlfuse harbour/
